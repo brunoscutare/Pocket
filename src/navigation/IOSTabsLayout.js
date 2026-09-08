c22efcf487df3e348c2e133d6ba5c2030502e91e
Bruno <brunobranco65@gmail.com>
2026-09-08 09:25:43 -0300

    arrumada versao de sdk

--- src/navigation/IOSTabsLayout.js
+++ src/navigation/IOSTabsLayout.js
@@ -1,24 +1,24 @@
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import { colors } from '../theme/colors';
 
 export function IOSTabsLayout() {
   return (
     <NativeTabs tintColor={colors.accent} minimizeBehavior="automatic" blurEffect="systemChromeMaterialDark">
       <NativeTabs.Trigger name="index">
-        <Icon sf={{ default: 'house', selected: 'house.fill' }} />
-        <Label>Início</Label>
+        <NativeTabs.Trigger.Icon sf={{ default: 'house', selected: 'house.fill' }} />
+        <NativeTabs.Trigger.Label>Início</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="pendencias">
-        <Icon sf={{ default: 'clock', selected: 'clock.fill' }} />
-        <Label>Pendências</Label>
+        <NativeTabs.Trigger.Icon sf={{ default: 'clock', selected: 'clock.fill' }} />
+        <NativeTabs.Trigger.Label>Pendências</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="movimentacoes">
-        <Icon sf={{ default: 'arrow.left.arrow.right', selected: 'arrow.left.arrow.right.circle.fill' }} />
-        <Label>Movimentações</Label>
+        <NativeTabs.Trigger.Icon sf={{ default: 'arrow.left.arrow.right', selected: 'arrow.left.arrow.right.circle.fill' }} />
+        <NativeTabs.Trigger.Label>Movimentações</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="configuracoes">
-        <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} />
-        <Label>Configurações</Label>
+        <NativeTabs.Trigger.Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} />
+        <NativeTabs.Trigger.Label>Configurações</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
